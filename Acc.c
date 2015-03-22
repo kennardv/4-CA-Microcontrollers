7ef16ea776e79194faeb918e4669be2d9fad9cb8
kevinheyvaert <kevin-heyvaert@hotmail.com>
2015-03-22 14:18:09 +0100

    First commit code ads en acc

--- Acc.c
+++ Acc.c
@@ -5,6 +5,12 @@ void AccInit(void)
 {
 		//uit slaapmodus halen :  By placing a high input signal on pin 7 of acc, the device will resume to normal mode of operation.
 		// --> ACC-sleep : PC6
+		
+		//set sleep pin of accelerometer
+		PORTC.OUT&=0b01000000;			//set PC6 high
+		PORTE.DIRSET=0b01000000;		//set PC6 as output (output = 1, input = 0)
+		
+		
 		//Poorten acc X, Y , Z staan op PA0 , PA1 en PA2 + GDN op PA4
 		
 		//PORTA.DIRCLR= 0b11110001;			//Set Input
