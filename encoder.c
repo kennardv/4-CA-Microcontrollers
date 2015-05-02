14339316300fd0ea5ba17422029cda452aa3ec2e
kevinheyvaert <kevin-heyvaert@hotmail.com>
2015-05-02 19:22:55 +0200

    Init encoder normaal af --> nog nakijken

--- encoder.c
+++ encoder.c
@@ -13,17 +13,18 @@ void EncoderInit(void)
 	//PORTC.PIN5CTRL = 0b00000000;		//totempole / sense on tboth edges
 	
 	//---------------------------------------------------------------------- > hieronder probeersel
-	
-	PORTC.PIN4CTRL = 0b01011001; //INVEN(1) + OPC(011) + ISC(000)
-	PORTC.PIN5CTRL = 0b01011001; //INVEN(1) + OPC(011) + ISC(000)
+	PORTC.DIRSET = 0b00110000;		//niet zeker		als output zetten 
 	
 	//PIN is inverted and detected on both edges and with pull-up
-	PORTC.INT0MASK = 0b00010000;
-	//Pin 4 is used as interrupt on mask0
-	PORTC.INT1MASK = 0b00100000;
-	//Pin 5 is used as interrupt on mask1
+	PORTC.PIN4CTRL = 0b01011001; //0 + INVEN(1) + OPC(011 = totem-pole pullup) + ISC(000 = both edges)	p.124
+	PORTC.PIN5CTRL = 0b01011001; //INVEN(1) + OPC(011) + ISC(000)
 	
-	//PORTC.INTFLAGS = 0b00000011; // INT1IF + INT0IF
+	//Pin 4 is used as interrupt on mask0		p.123
+	PORTC.INT0MASK = 0b00010000;
+	//Pin 5 is used as interrupt on mask1
+	PORTC.INT1MASK = 0b00100000;
+	
+	//PORTC.INTFLAGS = 0b00000011; // INT1IF + INT0IF			p.131  --> moet dit??
 	PORTC.INTCTRL = 0b00001010;	//INT1LVL(10) + INT0LVL(10)µ
 	
 	//ISR schrijven + globale variabele (volatile)
@@ -32,7 +33,7 @@ void EncoderInit(void)
 }
 int EncoderGetPos(void)
 {
-
+	int positie = 0;
 	// terugeven van encoder positie
-	return 0;
+	return positie;
 }
