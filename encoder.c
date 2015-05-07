5bff76a0ff591bef920ca7854af8ebf36f845843
kevinheyvaert <kevin-heyvaert@hotmail.com>
2015-05-07 13:12:32 +0200

    verdere uitwerking encoder

--- encoder.c
+++ encoder.c
@@ -7,7 +7,7 @@
 #include "encoder.h"
 
 volatile int Vorigepositie;
-volatile int Huidigepositie;
+volatile int Relatievepositie;
 
 void EncoderInit(void)
 {
@@ -23,9 +23,9 @@ void EncoderInit(void)
 	PORTC.PIN5CTRL = 0b01011001; //INVEN(1) + OPC(011) + ISC(000)
 	
 	//Pin 4 is used as interrupt on mask0		p.123
-	PORTC.INT0MASK = 0b00010000;
+	PORTC.INT0MASK = 0b00110000;
 	//Pin 5 is used as interrupt on mask1
-	PORTC.INT1MASK = 0b00100000;
+	//PORTC.INT1MASK = 0b00100000;
 	 
 	//PORTC.INTFLAGS = 0b00000011; // INT1IF + INT0IF			p.131  --> moet dit??
 	PORTC.INTCTRL = 0b00001010;	//INT1LVL(10) + INT0LVL(10)µ
@@ -36,7 +36,14 @@ void EncoderInit(void)
 
 ISR(PORTC_INT0MASK) //of toch PORTC_INT1_vect
 {
+	int result = (PORTC.IN & 0b00110000) >> 4;
 	
+	//switch case
+	//if
+	//else
+	//00 , 01 , 10 , 11
+	
+	Vorigepositie = result;
 }
 
 ISR(PORTC_INT1MASK)	//of toch PORTC_INT1_vect?
@@ -50,6 +57,5 @@ int EncoderGetPos(void)
 	//int positie = 0;
 	// terugeven van encoder positie
 	
-	int result = (PORTC.IN & 0b00110000) >> 4;
-	return result;
+	return Relatievepositie;
 }
